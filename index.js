b73baae34df7821bc5e288a654d554080e53fc11
LukasTopo <58187475+LucasLiSan@users.noreply.github.com>
2024-09-28 19:53:26 -0300

    testing verce lindex.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const app = express();
 
 /* ----------  DEFINIÇÕES BÁSICAS ---------- */
 app.set('view engine', 'ejs');
+app.set('views', './views');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
