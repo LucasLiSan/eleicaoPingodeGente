241d5eb4f3018b5e6bd7be1029783b1fb10f1e7f
LucasLiSan <lukassantana_rgt@hotmail.com>
2024-10-18 09:10:40 -0300

    update for render

--- controllers/resultsController.js
+++ controllers/resultsController.js
@@ -1,10 +1,10 @@
 import CandidateService from "../services/candidateService.js";
 
 // Defina o número total de eleitores
-const totalEleitores = 103;
+const totalEleitores = 86;
 
 // Define o total de seções(salas) e valor inicial de urnas apuradas para o calculo de apuração de urnas
-const totalRooms = 6; // Total de salas
+const totalRooms = 5; // Total de salas
 let urnasApuradas = 0; //Valor inicial igual a 0
 
 const resultados = async (req, res) => {
